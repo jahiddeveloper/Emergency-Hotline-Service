de3d2ff68de66ec8e271c4a573b9748312091178
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-11-03 11:17:53 +0600

    minus coin is compleat

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// Heart count
 let heartIcons = document.getElementsByClassName("heart-icon");
 
 for(let heartIcon of heartIcons) {
@@ -9,5 +9,30 @@ for(let heartIcon of heartIcons) {
         let currentCount = heartCount + 1;
 
         document.getElementById("heart-count").innerText = currentCount;
+    })
+}
+
+// Call btn
+
+let callBtns = document.getElementsByClassName("call-btn")
+for(let callBtn of callBtns) {
+    callBtn.addEventListener('click', function() {
+        
+        let cartTitle = callBtn.parentNode.children[1].innerText;
+        let cartHotline = callBtn.parentNode.children[3].innerText;
+
+        let coin = parseInt(document.getElementById("coin").innerText);
+        let subsCoin = parseInt(20);
+
+        if(coin < 20) {
+            alert("You don’t have enough coins. You need 20 coins to make a call.")
+            return;
+        }
+
+        alert (`Calling ${cartTitle} ${cartHotline}`);
+        
+        let currentCoin = coin - subsCoin;
+        
+        document.getElementById("coin").innerText = currentCoin;
     })
 }
